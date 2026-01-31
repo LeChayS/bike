40bd7d2e9dabaaabde12ade193d9eba0ee2bc0da
Lê Thị Tuyết Trâm <142717464+LeChayS@users.noreply.github.com>
2026-01-31 11:27:42 +0700

    refactor/ui_fix

--- Program.cs
+++ Program.cs
@@ -83,6 +83,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=BaoCao}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
